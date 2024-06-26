04be86c48cbde8db5fa8f20e76e0da8bde754118
PITCHAYAKIT-LEG\Pitchayakit <pitchayakit.p@gmail.com>
2024-06-26 13:48:51 +0700

    Add JWT to the login route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,24 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+//import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private authService: AuthService,
+    ) {}
 
     @Get()
     getHello(): string {
         return this.appService.getHello();
     }
 
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     @Post('auth/login')
     async login(@Request() req) {
-        return req.user;
+        return this.authService.login(req.user);
     }
 }
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: '8T1pL3UDqAt4rz6s2m5v9Dx8f9YmEfrW', //TODO: Move to .env
+};
