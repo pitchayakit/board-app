95fe32b7d334edc4d4a66c2280c9573c81f01d78
PITCHAYAKIT-LEG\Pitchayakit <pitchayakit.p@gmail.com>
2024-08-08 13:37:45 +0700

    Fix wrong migration timeline

--- src/migrations/1722830456046-CreateCommentTable.ts
+++ src/migrations/1722830456046-CreateCommentTable.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateCommentsTable1634567890123 implements MigrationInterface {
+export class CreateCommentsTable1722830456046 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
